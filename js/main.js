2048854d006786d9839f0218a28210c1b354e149
faroque-miah <faroquemaih99009@gmail.com>
2022-05-12 00:15:36 +0600

    syrah psd add

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ $(function(){
+    $('.feed_slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows:false
+      });
+ })
+   
+             
+ 
+ 
+ 
